d9c5398e76e9ff5ea9d4bd9a1a133baec2d4074e
Girma Nigusse <xgirma@users.noreply.github.com>
2020-03-29 16:28:58 -0400

    Update sandbox.spec.ts

--- cypress/integration/sandbox.spec.ts
+++ cypress/integration/sandbox.spec.ts
@@ -4,9 +4,12 @@ describe("Sandbox", () => {
   });
 
   it("should be on Sandbox", async () => {
-    const title = await cy.title();
-    const header = await cy.get("h1");
-    expect(title).to.eq("Sandbox");
-    expect(header).to.eq("Sandbox");
+    cy.title().then((title) => {
+      expect(title).to.eq("Sandbox");
+    });
+    
+    cy.get("h1").then((header) => {
+      expect(header).to.eq("Sandbox");
+    });
   });
 });
