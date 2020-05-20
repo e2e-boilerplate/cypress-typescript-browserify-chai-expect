108e161f057a79ec206e901018c81cce4e988e33
Girma Nigusse <xgirma@users.noreply.github.com>
2020-05-19 21:36:26 -0400

    Update sandbox.spec.ts

--- cypress/integration/sandbox.spec.ts
+++ cypress/integration/sandbox.spec.ts
@@ -3,7 +3,7 @@ describe("Sandbox", () => {
     cy.visit("https://e2e-boilerplate.github.io/sandbox/");
   });
 
-  it("should be on Sandbox", async () => {
+  it("should be on Sandbox", () => {
     cy.title().then((title) => {
       expect(title).to.eq("Sandbox");
     });
